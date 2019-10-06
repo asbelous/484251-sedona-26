de38be8a28b2a75437fd7e29684f1eeaebb6f742
Алексей Белоусов <arcaim@list.ru>
2019-10-06 21:22:10 +0700

    Небольшие изменения

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 var search = document.querySelector(".search");
 var modal = document.querySelector(".search-form .wrap");
-var form = document.querySelector("form");
-var form = modal.querySelector("form");
 var arrivalDate = modal.querySelector("#arrival-date");
 var departureDate = modal.querySelector("#departure-date");
 var numberOfAdults = modal.querySelector("#number-of-adults");
@@ -22,7 +20,6 @@ try {
 search.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.toggle("modal");
-  // form.classList.add("modal");
   arrival_date.focus();
   if (storageNumberOfAdults) {
     numberOfAdults.value = storageNumberOfAdults;
