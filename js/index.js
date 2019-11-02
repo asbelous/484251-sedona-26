6aef4d1eb4997992c6491c085857e3bbb455fe55
Алексей Белоусов <arcaim@list.ru>
2019-11-02 19:47:22 +0700

    Критерий Д17

--- js/index.js
+++ js/index.js
@@ -1,40 +1,18 @@
 var search = document.querySelector(".search");
 var modal = document.querySelector(".search-form .wrap");
-var arrivalDate = modal.querySelector("#arrival-date");
-var departureDate = modal.querySelector("#departure-date");
-var numberOfAdults = modal.querySelector("#number-of-adults");
-var numberOfChildren = modal.querySelector("#number-of-children");
-var storageNumberOfAdults = localStorage.getItem("numberOfAdults");
-var storageNumberOfChildren = localStorage.getItem("numberOfChildren");
-
-var isStorageSupport = true;
-var storage = "";
 
 try {
-  storage = localStorage.getItem("number-of-adults");
+  modal.classList.remove("modal");
+  search.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.toggle("modal");
+    arrival_date.focus();
+  });
 } catch (err) {
-  isStorageSupport = false;
+  modal.classList.remove("modal");
+  search.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.toggle("modal");
+    arrival_date.focus();
+  });
 }
-
-modal.classList.remove("modal");
-
-search.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.toggle("modal");
-  arrival_date.focus();
-  if (storageNumberOfAdults) {
-    numberOfAdults.value = storageNumberOfAdults;
-  }
-  if (storage_number_of_children) {
-    numberOfChildren.value = storageNumberOfChildren;
-  }
-});
-
-form.addEventListener("submit", function (evt) {
-  if (arrivalDate.value || departureDate.value) {
-    if (isStorageSupport) {
-      localStorage.setItem("numberOfAdults".staorageNumberOfAdults.value);
-      localStorage.setItem("numberOfChildre", storageNumberOfChildren.value);
-    }
-  }
-});
